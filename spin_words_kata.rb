54bbe53789edc3efad379b31e5ec0d71659bac57
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-01 17:44:45 +0100

    new spin kata completed

--- spin_words_kata.rb
+++ spin_words_kata.rb
@@ -1,3 +1,9 @@
-def spin_words
+#Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed.
 
+def spin_word(string)
+  string.split(' ')
+    .each { |word|  word.reverse! if word.length >= 5}
+    .join(' ')
 end
+
+p spin_word('this is a str')
