73dcab0574c3a84905264d91e06859c384be6dd1
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-14 11:37:24 +0100

    inital solutions

--- lewagon/louchebem.rb
+++ lewagon/louchebem.rb
@@ -1,11 +1,24 @@
-def louchebem(str)
-  arr = []
-  str.split('').each do |c| 
-    if c =~ /[aeiou]/
-      arr.push(c)
-    end
+def louchebem_word(str)
+  if str.length > 1
+    suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+    res = ''
+    arr = []
+
+    str.split('').each_with_index { |c, i| c =~ /[aeiou]/ ? arr << i : nil }
+
+    res = "l#{str[arr[0]..-1]}#{str[arr[0] -1]}#{suffix.sample}"
+    vres = "l#{str[arr[0]..-1]}#{suffix.sample}"
+
+    str[0] =~ /[aeiou]/ ? vres : res
+  else
+    str
   end
-  arr
 end
 
-p louchebem('patron')
+def louchebem(sentence)
+  res = []
+  sentence.split(' ').each { |w| res << louchebem_word(w) }
+  res.join(' ')
+end
+
+p louchebem('chat, fou!')
