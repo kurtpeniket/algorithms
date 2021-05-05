9d7898acb7194d19d797686bbeb6c4d4c1cec6ad
Kurt Peniket <kurt.peniket@gmail.com>
2021-05-05 19:05:13 +0100

    updated solutions

--- first_non_repeating.js
+++ first_non_repeating.js
@@ -1,8 +1,39 @@
+//My initial solution
+
 function firstNonRepeating(s) {
-  const arr = s.split('');
-  for(let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+  if (s === undefined) {
+    return '';
+  } else {
+
+    const arr = s.split('');
+    let obj = {};
+    
+    for(let i = 0; i < arr.length; i++) {
+      if (!obj.hasOwnProperty(arr[i].toLowerCase())) {
+        obj[arr[i]] = 1;  
+      } else if (obj.hasOwnProperty(arr[i].toLowerCase())) {
+        obj[arr[i]]++;
+      } else {
+        obj[arr[i]] = '';
+      }
+      console.log(obj);
+    }
+
+    for(const [k, v] of Object.entries(obj)) {
+      if (v == 1) {
+        return k;
+      }
+    }
   }
 }
 
-firstNonRepeating('string');
+//The actual solution
+
+function firstNonRepeatingLetter(s) {
+  for(var i in s) {
+    if(s.match(new RegExp(s[i],"gi")).length === 1) {
+      return s[i];
+    }
+  }
+  return '';
+}
